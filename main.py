a0aa4e1385b701c99d87f31e00f00e95d8d474f9
Bhladney26 <88292296+Bhladney@users.noreply.github.com>
2021-08-03 15:03:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+name = input("what is your name?")
+print(name)
+
+
+x = 10
+print(x)
+
+
+
+
+
+for i in range(5):
+  number = int(input("Type a number 1-50"))
+  if number > 10:
+    print("The number is greater than 10.")
+  elif number < 10:
+    print("The number is less than 10")
+  else: 
+      print("The number is equal to 10")
+
+
+for i in range(3):
+  evenorodd = int(input("Type a number"))
+  if evenorodd% 2 == 0:
+    print("your number's even")
+  else:
+    print("your number's odd")
+
+
+
+list = [1,2,3,4,5,6]
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[5])
+
+
+
+boolean = True
+
+def letters():
+  for numb in range():
+    string = input("type a word")
+    if string == "stop":
+      quit
+    else:
+       for i in string:
+        print(i)
+
+
+while boolean == True:
+    letters()
+
+
+
+
+#num = int(input("type a number"))
+#list = [1,num]
+#for i in range(num):
+  #if i < num-1:
+    #print(i+1)
+    #print("number is less than "num)
